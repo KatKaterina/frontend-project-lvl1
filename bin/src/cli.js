11dc31f8c9359035f18db8fffec53820460c6bd0
Salnikova Katerina <katya-salnikova@mail.ru>
2021-09-30 19:53:19 +0300

    add files

--- bin/src/cli.js
+++ bin/src/cli.js
@@ -1,7 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const toGreet = () => {
+const toGreet = () => {
   /* console.log('Welcome to the Brain Games!'); */
   const name = readlineSync.question('Your answer: ');
   console.log(`Hello, ${name}!`);
 };
+export default toGreet;
